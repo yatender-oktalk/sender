fa55cb9d731945295a2fae9247ad634365b50c91
yatender-oktalk <yatender.nitk@gmail.com>
2021-02-08 10:18:53 +0530

    first commit

---
+++ lib/sender.ex
@@ -0,0 +1,33 @@
+defmodule Sender do
+  @moduledoc """
+  Documentation for `Sender`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Sender.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def send_email("konnichiwa@world.com" = email),
+    do: raise("Oops, couldn't send email to #{email}!")
+
+  def send_email(email) do
+    Process.sleep(3000)
+    IO.inspect("Email to #{email} sent")
+    {:ok, "email sent"}
+  end
+
+  def notify_all(emails) do
+    Sender.EmailTaskSupervisor
+    |> Task.Supervisor.async_stream_nolink(emails, &send_email/1)
+    |> Enum.to_list()
+  end
+end
