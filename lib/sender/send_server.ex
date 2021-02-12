6002ce5c1a49c550c286f1881d289031c86f838b
yatender-oktalk <yatender.nitk@gmail.com>
2021-02-12 13:55:20 +0530

    Added retry send

--- lib/sender/send_server.ex
+++ lib/sender/send_server.ex
@@ -7,6 +7,7 @@ defmodule SendServer do
     state = %{emails: [], max_retries: max_retries}
     # To continue fetching from the database after init \
     # use the callback via handle continue
+    Process.send_after(self(), :retry, 5000)
     {:ok, state, {:continue, :fetch_from_database}}
   end
 
@@ -20,9 +21,37 @@ defmodule SendServer do
   end
 
   def handle_cast({:send, email}, state) do
-    Sender.send_email(email)
-    state = [%{email: email, status: "sent", retries: 0}] ++ state.emails
+    status =
+      case Sender.send_email(email) do
+        {:ok, "email sent"} -> "sent"
+        :error -> "failed"
+      end
 
-    {:noreply, state}
+    emails = [%{email: email, status: status, retries: 0}] ++ state.emails
+    IO.inspect(emails)
+    {:noreply, Map.put(state, :emails, emails)}
+  end
+
+  def handle_info(:retry, state) do
+    {failed, done} =
+      Enum.split_with(state.emails, fn item ->
+        item.status == "failed" && item.retries < state.max_retries
+      end)
+
+    retried =
+      Enum.map(failed, fn item ->
+        IO.puts("retrying email #{item.email}..")
+
+        new_status =
+          case Sender.send_email(item.email) do
+            {:ok, "email sent"} -> "sent"
+            _ -> "failed"
+          end
+
+        %{email: item.email, status: new_status, retries: item.retries + 1}
+      end)
+
+    Process.send_after(self(), :retry, 5_000)
+    {:noreply, Map.put(state, :emails, retried ++ done)}
   end
 end
